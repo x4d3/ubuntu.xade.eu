407d92fa9b475e5f15fea4202a8afc30815f7fc3
Xavier Delamotte <xavier.delamotte@red-badger.com>
2018-11-27 21:49:26 +0000

    fixup! Remove spring + Add healthcheck

--- config.rb
+++ config.rb
@@ -11,21 +11,19 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 # Min and Max threads per worker
 threads threads_count, threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
 
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix:///tmp/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "/var/log/rails/puma.stdout.log", "/var/log/rails/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "/tmp/puma.pid"
+state_path "/tmp/puma.state"
 activate_control_app
 
 # Allow puma to be restarted by `rails restart` command.
